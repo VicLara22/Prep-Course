5dd50757e33079f8f78aaccb6475a23b69746f80
VicLara22 <lara.vic.moyano@gmail.com>
2022-04-05 19:23:13 -0300

    Finish Homework 1.

---
+++ 02-JS-I/homework/homeworkactividad1.js
@@ -0,0 +1,14 @@
+/* En un archivo de texto separado que debes crear, escribe explicando los siguientes conceptos como si se lo estuviera explicando a un niño 
+de 12 años. Hacer esto te ayudará a descubrir rápidamente cualquier agujero en tu comprensión.
+a.Variables: Es la manera en que un programador le da nombre a un valor para reusarlo, actualizarlo o registrarlo. Se puede guardar
+cualquier tipo de datos.
+b.Instrumentos de cuerda: Es una cadena, el cual puede ser cualquier texto entre comilla.
+c.Funciones (argumentos, return): Es un bloque de codigo diseñado para realiar una tarea en particular. Se ejecuta cuando algo la invoca. Los
+argumentos de la funcion son los valores relaes recibidos por la funcion cuando se invoca.
+Dentro de la funcion, los argumentos se comportan como variables locales. El retorno es cuando devuelve algo.
+d.Declaracionesif: Es una estructura de control utilizada para tomar desiciones. Es un condicional que srive para realizar unas u otras 
+opreaciones en funcion de una expresion. Funciona de la sigueiente manera, primero se evalua una expresion, si da resutldao positivo se relizan 
+las acciones relacionadas con el caso positivo.
+e.Valores booleanos ( true, false): una variable es booleana cuando se le asigna como contenido un true o false. Estas variables se utilizan 
+para guardar informacion del tipo si/no, verdadero/falso informacion que unicamente puede tener dos estados
+*/
