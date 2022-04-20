518f90cb57f9018f8c98b55a95e7fef395ddd251
VicLara22 <lara.vic.moyano@gmail.com>
2022-04-19 22:11:48 -0300

    Finish homework

---
+++ 03-JS-II/homework/homework.puntouno.js
@@ -0,0 +1,6 @@
+/* 1. En un archivo de texto separado que debes crear, escribe explicaciones de los siguientes conceptos como si se lo estuvieras explicando 
+a un niño de 12 años. Hacer esto te ayudará a descubrir rápidamente cualquier agujero en tu comprensión.
+
+* `for`: se usa para actualizar o incrementar la variable contador. Una sentencia que se ejecuta mientras la condición se evalúa como verdadera
+* `&&`, `||`, `!`: el primero se entiende como "Y"; el segundo como "O"; y el ultimo como "NO"
+*/
